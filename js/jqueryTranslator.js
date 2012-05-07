1aa00cfa0f9847ca583fff12ffb54e57eafab6ef
Antonio Laguna <sombragriselros@gmail.com>
2012-05-07 17:28:17 +0200

    1.0.2 update
    Completed documentation and support for oforceLang and oasyncLangLoad

--- js/jqueryTranslator.js
+++ js/jqueryTranslator.js
@@ -3,34 +3,33 @@
 	Author: Antonio Laguna
 	Twitter: @Belelros
 	Website: http://www.funcion13.com
-	Version: 1.0
+	Version: 1.0.2
 */
 (function($, window, document, undefined){
     var Translate = {
         initialize : function(pkg, options){
-            var self = this;
-            self.packages = [];
-            self.loaded = $.Deferred();
+            this.packages = [];
+            this.loaded = $.Deferred();
 
-            self.translatable = true;
+            this.translatable = true;
 
-            self.options = $.extend({}, $.fn.jqTranslate.options, options);
-			var userLanguage = (self.options.forceLang) ? self.options.forceLang : self.getUserLanguage();
+            this.options = $.extend({}, $.fn.jqTranslate.options, options);
+            var userLanguage = this.getUserLanguage();
             
             if (typeof pkg === 'string')
-                self.packages.push(pkg);
+                this.packages.push(pkg);
             else
-                self.packages = pkg;
+                this.packages = pkg;
 
-            if (self.isTranslatable(userLanguage))
-                self.loadLanguages();
+            if (this.isTranslatable(userLanguage))
+                this.loadLanguages();
             else
-                self.translatable = false;
+                this.translatable = false;
 
-            return self.loaded.promise();
+            return this.loaded.promise();
         },
         getUserLanguage : function(){
-            var userLang = navigator.language || navigator.userLanguage;
+            var userLang = this.options.forceLang || navigator.language || navigator.userLanguage;
             userLang = userLang.replace(/_/, '-').toLowerCase();
 
             if (userLang.length > 3){
@@ -78,7 +77,8 @@
             return $.ajax ({
                 url : url,
                 dataType : "json",
-                cache : self.options.cache
+                cache : self.options.cache,
+		async: self.options.asyncLangLoad
             });
         },
         storeLangFile : function(data){
@@ -133,6 +133,7 @@
         skip : [],
         cache : true,
         onComplete : null,
-        forceLang : null
+        forceLang : null,
+		asyncLangLoad : true
     };
 })(jQuery, window, document);
